f17bb951cc6c1894b00ca65c3375c8e3ee13da28
Nemo Thorx <nemo@thorx.net>
2023-10-16 00:35:58 +1000

    updated script but not tested. readme added

--- noise.sh
+++ noise.sh
@@ -1,28 +1,54 @@
 #!/bin/bash
 
-# verbatim or derived from stuff at:
-# http://unreasonable.org/white_noise_generator_with_sox_for_Linux
-# and written up by me here
-# https://www.facebook.com/nemothorx/posts/10154560062528216?pnref=story
-# as well as discussed on #tron (15 Sept 2016)
+target=${1:-help}   # target noise
+dur=$2      # duration
+
+playopts="-q"   # global options to play. -q for quiet
 
 
-# beach
-play -n -n --combine merge synth $len pinknoise band -n 1200 1800 tremolo 50 10 tremolo 0.14 70 tremolo 0.2 50
-play -r 44100 -n -r 44100 -n --combine merge synth pinknoise band -n 1200 1800 tremolo 50 10 tremolo 0.14 70 tremolo 0.2 50 earwax
+case $target in
+    beach1)     # beach v1
+        play $playopts -n -n --combine merge synth $dur pinknoise band -n 1200 1800 tremolo 50 10 tremolo 0.14 70 tremolo 0.2 50
+        ;;
+    beach|beach2)   # beach v2
+        play $playopts -r 44100 -n -r 44100 -n --combine merge $dur synth pinknoise band -n 1200 1800 tremolo 50 10 tremolo 0.14 70 tremolo 0.2 50 earwax
+        ;;
+    train1)     # steam train v1
+        play $playopts -t sl - synth $dur pinknoise band -n 1200 200 tremolo 7 5 tremolo 5 75 tremolo 3 5 tremolo .1 20 tremolo .2 20 tremolo .3 20 tremolo .1 20 tremolo .01 30 < /dev/zero
+        ;;
+    train|train2)   # steam train v2
+        play $playopts -r 44100 -n -r 44100 -n --combine merge synth $dur pinknoise band -n 1200 200 tremolo 7 5 tremolo 5 75 tremolo 3 5 tremolo .1 20 tremolo .2 20 tremolo .3 20 tremolo .1 20 tremolo .01 30 earwax
+        ;;
+    storm1|rain1)   # rain from a storm v1
+        play $playopts -t sl - synth $dur pinknoise band -n 1200 200 tremolo 7 5 tremolo 5 5 tremolo 3 5 tremolo .1 20 tremolo .2 20 tremolo .3 20 tremolo .1 20 tremolo .01 30 < /dev/zero
+        ;;
+    storm|rain|storm2|rain2)   # rain from a storm v2
+        play $playopts -n synth $dur pinknoise band -n 1200 200 tremolo 7 5 tremolo 5 5 tremolo 3 5 tremolo .1 20 tremolo .2 20 tremolo .3 20 tremolo .1 20 tremolo .01 30
+        ;;
+    enterprise1)  # TNG Starship Enterprise v1 (h/t Neale)
+        play -q -c2 -n synth $dur whitenoise band -n 80 40 band -n 310 50 gain +15
+        ;;
+    enterprise2)    # TNG Starship Enterprise v2 (h/t Neale)
+        play -q -c2 -n synth $dur whitenoise band -n 220 50 band -n 80 40 gain +15
+        ;;
+    enterprise3)    # TNG Starship Enterprise v3 (h/t Neale)
+        play $playopts -n -c1 synth $dur whitenoise lowpass -1 120 lowpass -1 120 lowpass -1 120 gain +16
+        ;;
+    enterprise|enterprise4) # TNG Starship Enterprise v4 (h/t Neale / Paul)
+        play -q -c2 -n synth $dur whitenoise band -n 100 24 band -n 300 100 gain +20
+        ;;
+    *)
+        echo "Invocation: '$0 [theme] [duration]', where theme is one of the following:"
+        egrep "^    [a-z0-9|]*\).*# " $0 | sed -e 's/)//1' | column -t -s'#'
+        echo "Duration is optional, in HH:MM:SS format. Defaults to play till interrupted"
 
-# steam train
-play -t sl - synth 15:00 pinknoise band -n 1200 200 tremolo 7 5 tremolo 5 75 tremolo 3 5 tremolo .1 20 tremolo .2 20 tremolo .3 20 tremolo .1 20 tremolo .01 30 < /dev/zero
-play -r 44100 -n -r 44100 -n --combine merge synth 15:00 pinknoise band -n 1200 200 tremolo 7 5 tremolo 5 75 tremolo 3 5 tremolo .1 20 tremolo .2 20 tremolo .3 20 tremolo .1 20 tremolo .01 30 earwax
-
-# rain from storm
-play -t sl - synth 15:00 pinknoise band -n 1200 200 tremolo 7 5 tremolo 5 5 tremolo 3 5 tremolo .1 20 tremolo .2 20 tremolo .3 20 tremolo .1 20 tremolo .01 30 < /dev/zero
-play -n synth 15:00 pinknoise band -n 1200 200 tremolo 7 5 tremolo 5 5 tremolo 3 5 tremolo .1 20 tremolo .2 20 tremolo .3 20 tremolo .1 20 tremolo .01 30
-
-# ticktock clock (wishlist)
-
-# TARDIS (wishlist)
-
+esac
 
 # NOTES
 # append 'vol 2' at the end to make louder. Can't go much beyond 3 without clipping
+
+# Wishlist
+# - ticktock clock
+# - TARDIS
+
+
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Be noisy
+
+But pleasantly
+
+This script uses sox (invoked as "play") to simulate various soothing ambient sounds via synthesising noise and manipulating it
+
+Original idea from http://unreasonable.org/white_noise_generator_with_sox_for_Linux
+
+Further discussed on facebook and IRC in 2016, and expanded again 2023
+
+
