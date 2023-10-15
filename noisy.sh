41fbb3d3dc1a50cf4a5c28c5b96ad7013843bdfd
Nemo Thorx <nemo@thorx.net>
2023-10-16 01:06:57 +1000

    improve readme and help output

--- noisy.sh
+++ noisy.sh
@@ -7,39 +7,42 @@ playopts="-q"   # global options to play. -q for quiet
 
 
 case $target in
-    beach1)     # beach v1
+    beach1)     #~ beach v1
         play $playopts -n -n --combine merge synth $dur pinknoise band -n 1200 1800 tremolo 50 10 tremolo 0.14 70 tremolo 0.2 50
         ;;
-    beach|beach2)   # beach v2
+    beach|beach2)   #~ beach v2
         play $playopts -r 44100 -n -r 44100 -n --combine merge $dur synth pinknoise band -n 1200 1800 tremolo 50 10 tremolo 0.14 70 tremolo 0.2 50 earwax
         ;;
-    train1)     # steam train v1
+    train1)     #~ steam train v1
         play $playopts -t sl - synth $dur pinknoise band -n 1200 200 tremolo 7 5 tremolo 5 75 tremolo 3 5 tremolo .1 20 tremolo .2 20 tremolo .3 20 tremolo .1 20 tremolo .01 30 < /dev/zero
         ;;
-    train|train2)   # steam train v2
+    train|train2)   #~ steam train v2
         play $playopts -r 44100 -n -r 44100 -n --combine merge synth $dur pinknoise band -n 1200 200 tremolo 7 5 tremolo 5 75 tremolo 3 5 tremolo .1 20 tremolo .2 20 tremolo .3 20 tremolo .1 20 tremolo .01 30 earwax
         ;;
-    storm1|rain1)   # rain from a storm v1
+    storm1|rain1)   #~ rain from a storm v1
         play $playopts -t sl - synth $dur pinknoise band -n 1200 200 tremolo 7 5 tremolo 5 5 tremolo 3 5 tremolo .1 20 tremolo .2 20 tremolo .3 20 tremolo .1 20 tremolo .01 30 < /dev/zero
         ;;
-    storm|rain|storm2|rain2)   # rain from a storm v2
+    storm|rain|storm2|rain2)   #~ rain from a storm v2
         play $playopts -n synth $dur pinknoise band -n 1200 200 tremolo 7 5 tremolo 5 5 tremolo 3 5 tremolo .1 20 tremolo .2 20 tremolo .3 20 tremolo .1 20 tremolo .01 30
         ;;
-    enterprise1)  # TNG Starship Enterprise v1 (h/t Neale)
+    enterprise1)  #~ TNG Starship Enterprise v1 (h/t Neale)
         play -q -c2 -n synth $dur whitenoise band -n 80 40 band -n 310 50 gain +15
         ;;
-    enterprise2)    # TNG Starship Enterprise v2 (h/t Neale)
+    enterprise2)    #~ TNG Starship Enterprise v2 (h/t Neale)
         play -q -c2 -n synth $dur whitenoise band -n 220 50 band -n 80 40 gain +15
         ;;
-    enterprise3)    # TNG Starship Enterprise v3 (h/t Neale)
+    enterprise3)    #~ TNG Starship Enterprise v3 (h/t Neale)
         play $playopts -n -c1 synth $dur whitenoise lowpass -1 120 lowpass -1 120 lowpass -1 120 gain +16
         ;;
-    enterprise|enterprise4) # TNG Starship Enterprise v4 (h/t Neale / Paul)
+    enterprise|enterprise4) #~ TNG Starship Enterprise v4 (h/t Neale / Paul) 
         play -q -c2 -n synth $dur whitenoise band -n 100 24 band -n 300 100 gain +20
         ;;
+    #) #~ Enterprise themes attempted to be tuned towards TV accuracy
     *)
-        echo "Invocation: '$0 [theme] [duration]', where theme is one of the following:"
-        egrep "^    [a-z0-9|]*\).*# " $0 | sed -e 's/)//1' | column -t -s'#'
+        echo "Invocation: '$0 [theme] [duration]'
+where theme is one of the following:"
+        # Any lines of code with [space][hash][tilde] are formatted into help via the following:
+        egrep "[ ]#~" $0 | sed -e 's/)//1 ; s/#~/~/g' | column -t -s'~'
         echo "Duration is optional, in HH:MM:SS format. Defaults to play till interrupted"
 
 esac
@@ -50,5 +53,4 @@ esac
 # Wishlist
 # - ticktock clock
 # - TARDIS
-
 
--- readme.md
+++ readme.md
@@ -1,11 +1,18 @@
-# Be noisy
+# Be (pleasantly) noisy
 
-But pleasantly
+This script uses sox (invoked as "play") to simulate various soothing ambient
+sounds via synthesising noise and manipulating it
 
-This script uses sox (invoked as "play") to simulate various soothing ambient sounds via synthesising noise and manipulating it
-
+# Thanks to
 Original idea from http://unreasonable.org/white_noise_generator_with_sox_for_Linux
 
 Further discussed on facebook and IRC in 2016, and expanded again 2023
+
+# Issues
+
+* Only minimal effort has been made to make the various sound themes consistent
+  in invocation options or style - and most important as an issue: volume
+* Many themes throw WARNs to stderr
+* Many themes have an unpleasant audio glitch at start
 
 
